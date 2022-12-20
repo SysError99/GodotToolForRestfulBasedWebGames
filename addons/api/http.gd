0c966d572f49acad10e8e7c4e2a6103f44795818
SysError99 <17522480+SysError99@users.noreply.github.com>
2022-12-20 15:32:50 +0700

    Fix misleading error log

--- addons/api/http.gd
+++ addons/api/http.gd
@@ -30,7 +30,7 @@ func _request_completed(result: int, status_code: int, headers: PoolStringArray,
 	queue_free()
 	if get_meta("import_pck", false) && status_code == 200:
 		if !ProjectSettings.load_resource_pack(download_file):
-			printerr('Cannot import resource pack of path %s, trying to redownload...' % get_meta('import_pck_path'))
+			printerr('Cannot import resource pack of path %s' % get_meta('import_pck_path'))
 			emit_signal_http_request_completed_error(1)
 			return
 		var imported_pcks := api.get_meta("imported_pcks", []) as Array
